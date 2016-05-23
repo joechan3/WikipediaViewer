8af06840f5eb8cd7142362a26f3337ef3d5716a7
Joseph Chan <joechan3@gmail.com>
2016-05-22 19:57:18 -0600

    Minor refactoring

--- js/script.js
+++ js/script.js
@@ -1,49 +1,46 @@
 $(document).ready(function() {
-
-  function getWikipediaResults(searchTerms,callback) {
+  function getWikipediaResults(searchTerms, callback) {
     var apiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerms + "&limit=10&namespace=0&format=json&warningsaserror=true&callback=?";
-    
-    $.getJSON(apiURL,function success(data){
+
+    $.getJSON(apiURL, function success(data) {
       callback(data, searchTerms);
     });
   }
 
-  function updateHTML(searchResults,searchWords) {
-    console.log("From update HTML: ", searchResults);
-    console.log("Number of results is " + searchResults[1].length);
-    
-    var searchResultHTML = '<div class="col-sm-6 col-sm-offset-3">'+
-                             '<div class="panel panel-default">'+
-                               '<div class="panel-heading"><a href="#" target="_blank">Panel heading without title</a></div>'+
-                               '<div class="panel-body">'+
-                               '</div>'+
-                             '</div>'+
-                            '</div>';
-    
+  function updateHTML(searchResults, searchWords) {
+    var searchResultHTML = "<div class='col-sm-6 col-sm-offset-3'>" +
+      "<div class='panel panel-default'>" +
+      "<div class='panel-heading'><a href='#' target='_blank'>Panel heading without title</a></div>" +
+      "<div class='panel-body'>" +
+      "</div>" +
+      "</div>" +
+      "</div>";
+
     //Remove old search result(s)
     $("#searchResults").children().remove();
-    
-    if (searchResults[1].length === 0){
+
+    if (searchResults[1].length === 0) {
       //For no search results
       $("#searchResults").append(searchResultHTML);
       $(".panel-heading").html("<p style='text-align:left'>Your search - " + searchWords + " - did not match any Wikipedia entries.</p>");
-      
-      $(".panel-body").html("<p style='text-align:left'>Suggestions:</p>" + 
-                                            "<ul style='text-align:left'>" + 
-                                              "<li>Make sure that all words are spelled correctly.</li>" + 
-                                              "<li>Try different keywords." +
-                                              "<li>Try more general keywords."+
-                                            "</ul>");
+      $(".panel-body").html(
+        "<p style='text-align:left'>Suggestions:</p>" +
+        "<ul style='text-align:left'>" +
+        "<li>Make sure that all words are spelled correctly.</li>" +
+        "<li>Try different keywords." +
+        "<li>Try more general keywords." +
+        "</ul>"
+      );
     } else {
       //Create a search result panel(s)
-      for (var i = 0; i < searchResults[1].length; i++){
+      for (var i = 0; i < searchResults[1].length; i++) {
         $("#searchResults").append(searchResultHTML);
         $(".panel-heading:eq(" + i + ") a").attr("href", searchResults[3][i]);
         $(".panel-heading:eq(" + i + ") a").html(searchResults[1][i]);
 
         //Limit summary to 140 characters (arbitrary decision)
-        if (searchResults[2][i].length > 140){
-          searchResults[2][i] = searchResults[2][i].slice(0,140) + " . . .";
+        if (searchResults[2][i].length > 140) {
+          searchResults[2][i] = searchResults[2][i].slice(0, 140) + " . . .";
         }
         $(".panel-body:eq(" + i + ")").html(searchResults[2][i]);
       }
@@ -57,20 +54,14 @@ $(document).ready(function() {
   $("input").on("keypress", function keyboardSearch(event) {
     //keyCode 13 is Enter/Return key
     if (event.which === 13 && $("input").val() !== "") {
-      getWikipediaResults($("input").val(),updateHTML);
+      getWikipediaResults($("input").val(), updateHTML);
     }
   });
 
   $("#searchButton").on("click", function mouseSearch() {
     if ($("input").val() !== "") {
-      getWikipediaResults($("input").val(),updateHTML);
+      getWikipediaResults($("input").val(), updateHTML);
     }
   });
 
 });
-
-//request api search results
-
-
-
-//update HTML
